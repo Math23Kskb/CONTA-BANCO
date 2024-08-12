2c77d80363a40883901e6fd853c0b8f0e0378319
MATHC <92028037+Math23Kskb@users.noreply.github.com>
2024-08-12 18:17:21 -0300

    Conta Terminal com interação ao Usuario com Scanner

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -29,5 +29,3 @@ public class ContaTerminal {
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque. ");
     }
 }
-
-
